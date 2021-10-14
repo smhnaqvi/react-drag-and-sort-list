568dacb07ae4f18d5d3801604f17da5166f79209
syed mohhamad hussain naqvi <smhnaqvi111@gmail.com>
2021-10-14 15:53:05 +0330

    add moment js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import react from "react";
 import ListComponent from "./components/List/List";
+import "./style.scss";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>list</h1>
+    <div className={"container"}>
+      <h5 className="pageTitle">list</h5>
       <ListComponent />
     </div>
   );
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,12 +1,48 @@
 import react from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import List from "../../store/ListStore";
+import "./style.scss";
+import moment from "moment";
 
 const ListComponent = () => {
   return (
-    <div>
-      {List.get().map((item) => (
-        <h3>{item.title}</h3>
-      ))}
+    <DragDropContext>
+      <Droppable droppableId="droppable-1" type="PERSON">
+        {(provided, _) => (
+          <div
+            className="list"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {List.get().map((item, i) => (
+              <Draggable key={i} draggableId={`draggable-${i}`} index={i}>
+                {(provided, snapshot) => (
+                  <div ref={provided.innerRef} {...provided.draggableProps}>
+                    <ListItem
+                      data={item}
+                      dragHandler={provided.dragHandleProps}
+                    />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+const ListItem = ({ data, dragHandler }) => {
+  return (
+    <div className={"item"} {...dragHandler}>
+      <img className={"icon"} src={data.icon} alt={data.title} />
+      <div className={"item-details"}>
+        <h5 className={"title"}>{data.title}</h5>
+        <h5 className={"description"}>{data.description}</h5>
+        <h5 className={"create-time"}>{moment(data.created_at).fromNow()}</h5>
+      </div>
     </div>
   );
 };
